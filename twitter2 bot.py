f90320d2d45ac234a1160da50a3ca858c0fd2d4b
goyalshaffi <isha31198@gmail.com>
2018-07-18 18:58:25 +0530

    updated2

--- twitter2 bot.py
+++ twitter2 bot.py
@@ -1,6 +1,7 @@
 
 from keys2 import consumer_key,consumer_secret,access_token,access_secret
 import tweepy
+import json
 from paralleldots import set_api_key,get_api_key
 from paralleldots import *
 from keys3 import api_key
@@ -26,23 +27,26 @@ def sentiments():
     sentiment_value = sentiment(text)
     print(sentiment_value["sentiment"])
 
+
 def compare():
-    narendramodi = input().split()
-    realdonaldtrump=input().spilt()
-    y=input().spilt()
-    z=input().spilt()
-   # w = input("enter the tweet 1 which we want to compare")
-    #x = input("enter the tweet 2 which we want to compare")
-    tweets1 = api.user_timeline(screen_name="narendramodi")
-    tweets2 = api.user_timeline(screen_name= "realdonaldtrump")
+    w = input("enter the usrr1 which we want to compare")
+    x = input("enter the user2 which we want to compare")
+    q = w.split()
+    r = x.split()
+    y = input("enter the word u want to search")
+    m=y.split()
+    z = input("enter the word u want to search")
+    n=z.split()
+    tweets1 = api.user_timeline(screen_name=q)
+    tweets2 = api.user_timeline(screen_name=r)
     s1 = ''
     s2 = ''
     for tweet in tweets1:
         s1 += tweet.text
-    print(s1.count(y))
+    print(s1.count(m))
     for tweet in tweets2:
         s2 += tweet.text
-    print(s2.count(z))
+    print(s2.count(n))
 
 
 def loc_lan_tz():
@@ -68,58 +72,65 @@ def top_usage():
 def tweet_message():
     update_status = api.update_status(status="Hello Everyone !")
     print(update_status)
+def jsons():
+    m=input("enter the hashtag u want to search")
+    x = api.search(m)
+    for tweet in x:
+        json_data = json.dumps(tweet._json)
+
+    print(json_data)
+
 
 def dis():
     print('''
-1.Retrieve Tweets
-2.Count the followers
-3.Determine the sentiment
-4.Compare tweets
-5.Determine location,language and time zone.
-6.Analyze top usage
-7.Tweet a message
-8.Exit
-'''
-)
+            1.Retrieve Tweets
+            2.Count the followers
+            3.Determine the sentiment
+            4.Compare tweets
+            5.Determine location,language and time zone.
+            6.Analyze top usage
+            7.Tweet a message
+            8.To convert in json
+            9.Exit
+            ''')
+
+
+    option=int(input("what option you want: "))
+
+    if(option==1):
+        retrive_tweets()
+        dis()
+
+    elif(option==2):
+        count()
+        dis()
+
+    elif(option==3):
+        sentiments()
+        dis()
+
+    elif(option==4):
+        compare()
+        dis()
+
+    elif(option==5):
+        loc_lan_tz()
+        dis()
+
+    elif(option==6):
+        top_usage()
+        dis()
+
+    elif(option==7):
+        tweet_message()
+        dis()
+
+    elif(option==8):
+        jsons()
+        dis()
+
+    else:
+        print("choose a correct option")
 dis()
-option=int(input("what option you want: "))
-if(option==1):
-    retrive_tweets()
-    dis()
-elif(option==2):
-    count()
-    dis()
-elif(option==3):
-    sentiments()
-    dis()
-elif(option==4):
-    compare()
-    dis()
-elif(option==5):
-    loc_lan_tz()
-    dis()
-elif(option==6):
-    top_usage()
-    dis()
-elif(option==7):
-    tweet_message()
-    dis()
-else:
-    print("choose a correct option")
 
 
-
-
-from keys2 import consumer_key,consumer_secret,access_token,access_secret
-import tweepy
-import json
-
-
-oauth=tweepy.OAuthHandler(consumer_key,consumer_secret)
-oauth.set_access_token(access_token,access_secret)
-api=tweepy.API(oauth)
-x=print(api.search("#sanju"))
-for tweet in x:
-    json_data=json.dumps(tweet._json)
-
-print(json_data)
